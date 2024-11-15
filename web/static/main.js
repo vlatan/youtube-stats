b848dd03217b0aa81c7c0d01cf93bba874b402a8
vlatan <33163699+vlatan@users.noreply.github.com>
2024-11-15 17:23:17 +0100

    Implement zoom on svg map

--- web/static/main.js
+++ web/static/main.js
@@ -62,4 +62,62 @@ document.addEventListener("DOMContentLoaded", () => {
             });
         });
     });
+
+    const svgImage = document.getElementById("svgImage");
+    const svgContainer = document.getElementById("svgContainer");
+    // const zoomValue = document.getElementById("zoomValue");
+    var vb = svgImage.getAttribute("viewBox").split(" ").map(parseFloat);
+    var viewBox = { x: vb[0], y: vb[1], w: vb[2], h: vb[3] };
+    const svgSize = { w: svgImage.clientWidth, h: svgImage.clientHeight };
+    var isPanning = false;
+    var startPoint = { x: 0, y: 0 };
+    var endPoint = { x: 0, y: 0 };
+    var scale = 1;
+
+    svgContainer.onmousewheel = e => {
+        e.preventDefault();
+        var w = viewBox.w;
+        var h = viewBox.h;
+        var mx = e.offsetX;
+        var my = e.offsetY;
+        var dw = w * Math.sign(e.deltaY) * 0.05;
+        var dh = h * Math.sign(e.deltaY) * 0.05;
+        var dx = dw * mx / svgSize.w;
+        var dy = dh * my / svgSize.h;
+        viewBox = { x: viewBox.x + dx, y: viewBox.y + dy, w: viewBox.w - dw, h: viewBox.h - dh };
+        scale = svgSize.w / viewBox.w;
+        // zoomValue.innerText = `${Math.round(scale * 100) / 100}`;
+        svgImage.setAttribute('viewBox', `${viewBox.x} ${viewBox.y} ${viewBox.w} ${viewBox.h}`);
+    }
+
+
+    svgContainer.onmousedown = e => {
+        isPanning = true;
+        startPoint = { x: e.x, y: e.y };
+    }
+
+    svgContainer.onmousemove = e => {
+        if (isPanning) {
+            endPoint = { x: e.x, y: e.y };
+            var dx = (startPoint.x - endPoint.x) / scale;
+            var dy = (startPoint.y - endPoint.y) / scale;
+            var movedViewBox = { x: viewBox.x + dx, y: viewBox.y + dy, w: viewBox.w, h: viewBox.h };
+            svgImage.setAttribute('viewBox', `${movedViewBox.x} ${movedViewBox.y} ${movedViewBox.w} ${movedViewBox.h}`);
+        }
+    }
+
+    svgContainer.onmouseup = e => {
+        if (isPanning) {
+            endPoint = { x: e.x, y: e.y };
+            var dx = (startPoint.x - endPoint.x) / scale;
+            var dy = (startPoint.y - endPoint.y) / scale;
+            viewBox = { x: viewBox.x + dx, y: viewBox.y + dy, w: viewBox.w, h: viewBox.h };
+            svgImage.setAttribute('viewBox', `${viewBox.x} ${viewBox.y} ${viewBox.w} ${viewBox.h}`);
+            isPanning = false;
+        }
+    }
+
+    svgContainer.onmouseleave = () => {
+        isPanning = false;
+    }
 });
